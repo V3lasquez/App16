5ea2966bf7b066afac0dedc7e537264f8951e38b
V3lasquez <noe.vc1782@gmail.com>
2023-12-21 16:22:00 -0500

    Agregar archivos de proyecto.

---
+++ App16/App16/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using App16.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App16
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string BaseUrl = "https://airbnb-api-mu.vercel.app";
+
+        // Declarar la colección placesList
+        private ObservableCollection<Place> placesList = new ObservableCollection<Place>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            // Establecer el ItemsSource del ListView en placesList
+            listView.ItemsSource = placesList;
+        }
+
+        public async Task<T> GetAsync<T>(string endpoint)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+
+                // Realizar la solicitud GET al servidor
+                HttpResponseMessage response = await client.GetAsync(endpoint);
+
+                // Verificar si la solicitud fue exitosa (código de estado 200)
+                if (response.IsSuccessStatusCode)
+                {
+                    // Leer y deserializar la respuesta JSON
+                    string json = await response.Content.ReadAsStringAsync();
+                    T result = JsonConvert.DeserializeObject<T>(json);
+                    return result;
+                }
+                else
+                {
+                    // Manejar errores
+                    throw new Exception($"Error en la solicitud: {response.StatusCode}");
+                }
+            }
+        }
+
+        private async void llamarAPI(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await GetAsync<ResponseBase>("airbnb");
+
+                // Actualiza la lista de lugares en la vista
+                placesList.Clear();
+                foreach (var place in result.places)
+                {
+                    placesList.Add(place);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores
+                DisplayAlert("Error", $"Error en la solicitud: {ex.Message}", "Aceptar");
+            }
+        }
+    }
+}
---
+++ App16/App16/Model/ResponseBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App16.Model
+{
+    public class Location
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+    }
+
+    public class Place
+    {
+        public string name { get; set; }
+        public double rating { get; set; }
+        public Location location { get; set; }
+        public string image_url { get; set; }
+        public string date { get; set; }
+        public double price { get; set; }
+        public double longitud { get; set; }
+        public double latitud { get; set; }
+    }
+
+    public class ResponseBase
+    {
+        public List<Place> places { get; set; }
+    }
+}
